325ae263a08efa1b5f3120969fd191c741279804
Mar1ami <mlomidze255@gmail.com>
2023-04-09 18:36:44 +0400

    day2 code commit

---
+++ day2/lesson2.py
@@ -0,0 +1,10 @@
+name="mariam"
+surname="lomidze"
+age=20
+height=171.5
+is_blonde=True
+is_evil=False
+good_bye=()
+print(type(good_bye))
+
+print(name +" "+ surname + " " + str(age) + " " +  str(height) + " " + str(is_blonde) + " " + str(is_evil) + " " + str(good_bye))
