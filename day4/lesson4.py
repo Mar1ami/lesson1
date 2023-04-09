9234977ade25224ded1c9ec0ff8a692d9c607089
Mar1ami <mlomidze255@gmail.com>
2023-04-09 17:55:08 +0400

    day4 code commit

---
+++ day4/lesson4.py
@@ -0,0 +1,33 @@
+name1=input("enter name1: ")
+name2=input("enter name2: ")
+ammount_of_consonants_in_name1 = 0
+ammount_of_consonants_in_name2 = 0
+for char in name1:
+    if char in "bcdfghjklmnpqrvxwz":
+        ammount_of_consonants_in_name1 += 1
+        
+          
+for char in name2:
+    if char in "bcdfghjklmnpqrvxwz":
+        ammount_of_consonants_in_name2 += 1
+        
+if ammount_of_consonants_in_name1 >= ammount_of_consonants_in_name2:
+     print("the ammount of consonants in name1 is more and it contains {} consonants".format
+  (ammount_of_consonants_in_name1))
+     
+#sum
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+num3 = int(input("enter num3: "))
+
+my_sum=0
+15,17,7
+if num1 % 2 == 1:
+    my_sum += num1
+if num2 % 2 == 1:
+    my_sum += num2
+if num3 % 2 == 1:
+    my_sum += num3
+
+print("the sum of odd numbers is {}".format(my_sum))
+
